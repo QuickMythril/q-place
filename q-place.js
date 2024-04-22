4fae676c7c428c23b69277227d00861edd946d91
QuickMythril <quickmythril@protonmail.com>
2024-04-22 02:24:18 -0400

    v0.2.0 - More publish data

--- q-place.js
+++ q-place.js
@@ -1,13 +1,33 @@
 document.addEventListener('DOMContentLoaded', fetchPixels);
 
 const gridSize = 100;
-let pixels = [
-    "4747ffffff", "4847e6db00", "4947a16a3f", "5047e79700", "514792e233",
-    "474800c000", "4848ffffff", "4948ffffff", "5048ffffff", "5148888789",
-    "474901e5f2", "4849ffffff", "4949000000", "5049ffffff", "5149e4e4e4",
-    "47500082ca", "4850ffffff", "4950ffffff", "5050ffffff", "5150ffa6d1",
-    "47510600ee", "4851e23eff", "4951820281", "5051ffffff", "5151e80000"
-];
+let pixels = {
+    "47-47": { color: "#ffffff", user: "", time: null },
+    "48-47": { color: "#e6db00", user: "", time: null },
+    "49-47": { color: "#a16a3f", user: "", time: null },
+    "50-47": { color: "#e79700", user: "", time: null },
+    "51-47": { color: "#92e233", user: "", time: null },
+    "47-48": { color: "#00c000", user: "", time: null },
+    "48-48": { color: "#ffffff", user: "", time: null },
+    "49-48": { color: "#ffffff", user: "", time: null },
+    "50-48": { color: "#ffffff", user: "", time: null },
+    "51-48": { color: "#888789", user: "", time: null },
+    "47-49": { color: "#01e5f2", user: "", time: null },
+    "48-49": { color: "#ffffff", user: "", time: null },
+    "49-49": { color: "#000000", user: "", time: null },
+    "50-49": { color: "#ffffff", user: "", time: null },
+    "51-49": { color: "#e4e4e4", user: "", time: null },
+    "47-50": { color: "#0082ca", user: "", time: null },
+    "48-50": { color: "#ffffff", user: "", time: null },
+    "49-50": { color: "#ffffff", user: "", time: null },
+    "50-50": { color: "#ffffff", user: "", time: null },
+    "51-50": { color: "#ffa6d1", user: "", time: null },
+    "47-51": { color: "#0600ee", user: "", time: null },
+    "48-51": { color: "#e23eff", user: "", time: null },
+    "49-51": { color: "#820281", user: "", time: null },
+    "50-51": { color: "#ffffff", user: "", time: null },
+    "51-51": { color: "#e80000", user: "", time: null }
+};
 let selectedPixelData = null;
 let userPublicKey = '';
 let userAddress = '';
@@ -24,7 +44,13 @@ async function fetchPixels() {
             results.sort((a, b) => (a.updated || a.created) - (b.updated || b.created));
             results.forEach(result => {
                 const pixelData = result.identifier.slice(13, 23);
-                pixels.push(pixelData);
+                const [x, y] = [pixelData.slice(0, 2), pixelData.slice(2, 4)];
+                const color = `#${pixelData.slice(4, 10)}`;
+                pixels[`${x}-${y}`] = {
+                    color,
+                    user: result.name,
+                    time: result.created
+                };
             });
         }
     } catch (error) {
@@ -40,7 +66,7 @@ function createGrid() {
         let newRow = document.createElement('tr');
         for (let x = 0; x < gridSize; x++) {
             let cell = document.createElement('td');
-            cell.setAttribute('id', `${x}-${y}`);
+            cell.setAttribute('id', `${x.toString().padStart(2, '0')}-${y.toString().padStart(2, '0')}`);
             cell.style.backgroundColor = "#1e90ff"; // DodgerBlue
             cell.addEventListener('click', handlePixelClick);
             newRow.appendChild(cell);
@@ -51,13 +77,10 @@ function createGrid() {
 }
 
 function colorPixels() {
-    for (const pixel of pixels) {
-        const pixelX = parseInt(pixel.slice(0, 2), 10); // Extract two characters
-        const pixelY = parseInt(pixel.slice(2, 4), 10); // Extract two characters
-        const pixelRGB = pixel.slice(4, 10); // Extract six characters
-        let pixelCell = document.getElementById(`${pixelX}-${pixelY}`);
+    for (const [coords, pixelData] of Object.entries(pixels)) {
+        const pixelCell = document.getElementById(coords);
         if (pixelCell) {
-            pixelCell.style.backgroundColor = `#${pixelRGB}`;
+            pixelCell.style.backgroundColor = pixelData.color;
         }
     }
 }
@@ -84,25 +107,22 @@ async function accountLogin() {
 
 async function handlePixelClick(event) {
     const cell = event.target;
-    const [x, y] = cell.id.split('-');
-    updateTopBar(x, y, cell.style.backgroundColor);
+    const coords = cell.id;
+    const [x, y] = coords.split('-').map(coord => coord.padStart(2, '0'));
+    updateTopBar(x, y, pixels[coords]?.color || '#1e90ff', pixels[coords]?.user, pixels[coords]?.time);
     const colorPicker = document.getElementById('color-picker');
-    selectedPixelData = `${x.padStart(2, '0')}${y.padStart(2, '0')}`;
+    selectedPixelData = `${x}-${y}`;
     const publishButton = document.getElementById('publish-button');
     publishButton.removeEventListener('click', publishSelectedPixel);
     publishButton.addEventListener('click', publishSelectedPixel);
 }
 
-function updateTopBar(x, y, color) {
+function updateTopBar(x, y, color, user, time) {
     document.getElementById('coordinates').textContent = `${x}, ${y}`;
     document.getElementById('current-color').textContent = `${color}`;
-    document.getElementById('color-picker').value = rgbToHex(color);
-}
-
-function rgbToHex(rgbColor) {
-    const rgbValues = rgbColor.match(/\d+/g);
-    const hexValues = rgbValues.map(value => parseInt(value).toString(16).padStart(2, '0'));
-    return `#${hexValues.join('')}`;
+    document.getElementById('color-picker').value = color;
+    document.getElementById('last-user').innerHTML = user ? `<img src="/arbitrary/THUMBNAIL/${user}/qortal_avatar" onerror="this.style='display:none'"> ${user}` : 'N/A';
+    document.getElementById('timestamp').textContent = time ? `${formatTimeAgo(time)}` : 'N/A';
 }
 
 async function publishSelectedPixel() {
@@ -110,19 +130,19 @@ async function publishSelectedPixel() {
         const colorPicker = document.getElementById('color-picker');
         const color = colorPicker.value;
         const rgbColor = color.substring(1);
-        const pixelData = selectedPixelData + rgbColor;
+        const [x, y] = selectedPixelData.split('-').map(coord => coord.padStart(2, '0'));
+        const pixelData = x + y + rgbColor;
         await publishPixel(pixelData);
-        const [x, y] = selectedPixelData.match(/.{1,2}/g);
-        const cell = document.getElementById(`${x}-${y}`);
+        const cell = document.getElementById(`${selectedPixelData}`);
         cell.style.backgroundColor = color;
     }
 }
 
 async function publishPixel(pixelData) {
     try {
-        if (!userName || userName === 'Name unavailable') {
+        if (!userName || userName === 'Name unavailable' || userName === '') {
             await accountLogin();
-            if (!userName || userName === 'Name unavailable') {
+            if (!userName || userName === 'Name unavailable' || userName === '') {
                 console.error('Username is still unavailable');
                 return;
             }
@@ -140,4 +160,38 @@ async function publishPixel(pixelData) {
     } catch (error) {
         console.error('Error publishing pixel:', error);
     }
+}
+
+function formatTimeAgo(timestamp) {
+    const currentTimestamp = Date.now();
+    timeAgo = currentTimestamp - timestamp;
+    if (timeAgo < 0) {
+        return "pending";
+    }
+    const seconds = Math.floor((timeAgo / 1000) % 60);
+    const minutes = Math.floor((timeAgo / (1000 * 60)) % 60);
+    const hours = Math.floor((timeAgo / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(timeAgo / (1000 * 60 * 60 * 24) % 30.4375);
+    const months = Math.floor(timeAgo / (1000 * 60 * 60 * 24 * 30.4375) % 12);
+    const years = Math.floor(timeAgo / (1000 * 60 * 60 * 24 * 30.4375 * 12));
+    let readableFormat = '';
+    if (years > 0) {
+        readableFormat += `${years} yr${years>1?'s':''}${months>0?', ':''}`;
+    }
+    if (months > 0) {
+        readableFormat += `${months} month${months>1?'s':''}${years<1&&days>0?', ':''}`;
+    }
+    if ((days > 0) && (years < 1)) {
+        readableFormat += `${days} day${days>1?'s':''}${months<1&&hours>0?', ':''}`;
+    }
+    if ((hours > 0) && (months+years < 1)) {
+        readableFormat += `${hours} hr${hours>1?'s':''}${days<1&&minutes>0?', ':''}`;
+    }
+    if ((minutes > 0) && (days+months+years < 1)) {
+        readableFormat += `${minutes} min${minutes>1?'s':''}${hours<1&&seconds>0?', ':''}`;
+    }
+    if ((seconds > 0) && (hours+days+months+years < 1)) {
+        readableFormat += `${seconds} sec${seconds>1?'s':''} `;
+    }
+    return `${readableFormat} ago`;
 }
